8df6b3b6b6d26b9271d6bae274a64e34837c9f88
joaomgcosta90 <joaomgcosta90@gmail.com>
2021-05-14 12:08:42 +0100

    3 types of Injected controllers and junit test classes

---
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -0,0 +1,39 @@
+package guru.springframework.sfgdi;
+
+import guru.springframework.sfgdi.controllers.ConstructorInjectedController;
+import guru.springframework.sfgdi.controllers.MyController;
+import guru.springframework.sfgdi.controllers.PropertyInjectedController;
+import guru.springframework.sfgdi.controllers.SetterInjectedController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SfgDiApplication {
+
+	public static void main(String[] args) {
+
+		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+
+		MyController myController = (MyController) ctx.getBean("myController");
+
+		String myControllerString = myController.sayHello();
+
+		System.out.println(myControllerString);
+
+		System.out.println("--------- Property Injected Controller");
+		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+		System.out.println(propertyInjectedController.getGreetings());
+
+		System.out.println("--------- Setter Injected Controller");
+		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
+		System.out.println(setterInjectedController.getGreetings());
+
+		System.out.println("--------- Constructor Injected Controller");
+		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+		System.out.println(constructorInjectedController.getGreetings());
+
+
+	}
+
+}
